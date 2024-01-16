28151ccb0ebb6b1c233009c9e4a7a8432b8ae63f
Izabelly <98113162+Izabelly-Melo@users.noreply.github.com>
2024-01-15 21:57:38 -0300

    creating CheckBox style

--- Front/src/components/ui/CheckBox/index.js
+++ Front/src/components/ui/CheckBox/index.js
@@ -4,8 +4,9 @@ export function CheckBox({ options }) {
     return (
         <div className={styles.container}>
             {options.map(option => {
-                return <label className={styles.checkbox}>{option.title}
+                return <label className={styles.checkbox}>
                     <input type="checkbox" id={option.id} />
+                    {option.title}
                 </label>
             })}
         </div>
