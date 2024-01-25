cd91b0d9fb2e9c244fcf20483bcfb661e72e1ada
Izabelly <98113162+Izabelly-Melo@users.noreply.github.com>
2024-01-24 21:59:48 -0300

    changes on login page

--- Front/src/components/ui/CheckBox/index.js
+++ Front/src/components/ui/CheckBox/index.js
@@ -1,11 +1,11 @@
 import styles from './styles.module.css'
 
-export function CheckBox({ options }) {
+export function CheckBox({ options, ...rest }) {
     return (
         <div className={styles.container}>
             {options.map(option => {
                 return <label key={`checkbox-opt-${option.id}`} className={styles.checkbox}>
-                    <input type="checkbox" id={option.id} />
+                    <input type="checkbox" id={option.id} { ...rest } />
                     {option.title}
                 </label>
             })}
--- Front/src/pages/index.js
+++ Front/src/pages/index.js
@@ -1,47 +1,67 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/home.module.css";
 
-import { Input, TextArea } from '../components/ui/Input'
-import { Button, ButtonGrid, ButtonMenu } from '../components/ui/Button'
-import { RadioButton } from '../components/ui/RadioButton'
-import { CheckBox } from '../components/ui/CheckBox'
-import { ComboBox } from '../components/ui/ComboBox'
+import { Input, TextArea } from "../components/ui/Input";
+import { Button, ButtonGrid, ButtonMenu } from "../components/ui/Button";
+import { RadioButton } from "../components/ui/RadioButton";
+import { CheckBox } from "../components/ui/CheckBox";
+import { ComboBox } from "../components/ui/ComboBox";
+import { useState } from "react";
 
 const loginOptions = [
   {
-    id: 'P',
-    title: "Professor"
+    id: "P",
+    title: "Professor",
   },
   {
-    id: 'A',
-    title: "Aluno"
+    id: "A",
+    title: "Aluno",
   },
-
-]
+];
 
 export default function Home() {
+
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
     <>
       <Head>
         <title>Login</title>
       </Head>
       <div className={styles.container}>
+        <RadioButton className={styles.radio} options={loginOptions} />
         <div className={styles.login}>
-          <RadioButton options={loginOptions} />
-          <form>
-            <Input placeholder="Nome de Usuário" />
-            <Input placeholder="Senha" />
-            <div className={[styles.textLeft, styles.linkPassword]}>
-              <a href='/reset_password'>Esqueci minha senha!</a>
-              <Button color="light-blue" content={
-                <a>Acessar</a>
-              } />
-            </div>
-          </form>
+          { 
+            true ? 
+            <form>
+              <Input placeholder="Nome de Usuário" type="text" />
+              <Input placeholder="Senha" type={showPassword? 'text' : 'password'} />
+              <div className={[styles.passwordContainer]}>
+                <CheckBox onChange={() => {
+                  setShowPassword(!showPassword)
+                }} options={[
+                    {
+                      id: "S",
+                      title: "Mostrar Senha",
+                    },
+                  ]}
+                ></CheckBox>
+                <a href="/reset_password" target="_blank" className={[styles.passwordReset]}>
+                  Esqueci minha senha
+                </a>
+              </div>
+                <Button color="light-blue" content={<span>Acessar</span>} />
+            </form>
+            :
+            <form>
+              <Input placeholder="Nome de Usuário" type="text" />
+              <Button color="light-blue" content={<span>Acessar</span>} />
+            </form>
+          }
         </div>
         <div className={styles.background}></div>
       </div>
     </>
-  )
+  );
 }
